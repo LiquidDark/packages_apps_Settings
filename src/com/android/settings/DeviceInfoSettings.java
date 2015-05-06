b954d3f7cd30d01d83c83c220ad6e280fa7af41e
beanstown106 <nbenis106@gmail.com>
2015-05-06 03:22:51 -0400

    Nougat version
    Change-Id: I2521a2d683312bd41449e7ab9316255c1f602e63

--- src/com/android/settings/DeviceInfoSettings.java
+++ src/com/android/settings/DeviceInfoSettings.java
@@ -69,7 +69,7 @@ public class DeviceInfoSettings extends SettingsPreferenceFragment implements In
     private static final String KEY_MOD_BUILD_COMPILER_GCC = "build_compiler_gcc";
     private static final String KEY_MOD_BUILD_COMPILER_CLANG = "build_compiler_clang";
     private static final String KEY_VENDOR_VERSION = "vendor_version";
-
+    private static final String KEY_NOUGAT_VERSION = "nougat_version";
     static final int TAPS_TO_BE_A_DEVELOPER = 7;
 
     long[] mHits = new long[3];
@@ -128,7 +128,8 @@ public class DeviceInfoSettings extends SettingsPreferenceFragment implements In
         setValueSummary(KEY_MOD_BUILD_DATE, "ro.build.date");
         setValueSummary(KEY_MOD_BUILD_COMPILER_GCC, "ro.build.uber.gcc");
         setValueSummary(KEY_MOD_BUILD_COMPILER_CLANG, "ro.build.uber.clang");
-
+        setValueSummary(KEY_NOUGAT_VERSION, "ro.nougat.version");
+        findPreference(KEY_NOUGAT_VERSION).setEnabled(true);
         if (!SELinux.isSELinuxEnabled()) {
             String status = getResources().getString(R.string.selinux_status_disabled);
             setStringSummary(KEY_SELINUX_STATUS, status);
